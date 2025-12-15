9bbbc20a6328913e55269c13ec7ee89ead6bfb66
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-12-15 23:11:11 +0100

    card style work in progress

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,30 +20,40 @@ export default function Home() {
 
 
     return (
-        <>
-            <div className="container movie-card-grid">
-                <div className="row row-cols-1 row-cols-lg-2 g-4">
 
-                    {/* Card 1 */}
-                    <div className="col">
-                        <div className="movie-card">
-                            <div className="movie-card-image">
-                                <img
-                                    src="https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1535759872/kuldar-kalvik-799168-unsplash.webp"
-                                    alt="Movie One"
-                                />
-                            </div>
-                            <div className="movie-card-body">
-                                <h5>Movie One</h5>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                                <a href="#!" className="btn-details">Details</a>
+        <ul> {
+
+            films.map((movie, index) => (
+
+                <div key={index} className="container movie-card-grid">
+                    <div className="row row-cols-1 row-cols-lg-2 g-4">
+
+
+                        <div className="col">
+                            <div className="movie-card">
+                                <div className="movie-card-image">
+                                    <img
+                                        src={movie.image}
+                                        alt=""
+                                    />
+                                </div>
+                                <div className="movie-card-body">
+                                    <h5>{movie.title}</h5>
+                                    <p>{movie.abstract}</p>
+                                    <a href="#!" className="btn-details">Details</a>
+                                </div>
                             </div>
                         </div>
+
+
                     </div>
-
-
                 </div>
-            </div>
-        </>
+
+
+            ))
+
+
+        }</ul>
+
     )
 }
