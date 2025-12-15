c78834f7a13982690b90391b7f79e30dcaa29bda
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-12-15 22:10:18 +0100

    add header with style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,48 @@
+import { Link, NavLink } from "react-router-dom";
+
+
 export default function Header() {
 
-    return (
 
+
+
+    return (
         <header>
-            <h1>HEEAAADEEERRR</h1>
+
+            <nav className="navbar navbar-expand-lg navbar-light bg-white py-3">
+                <div className="container px-5">
+                    <a className="navbar-brand" href="#">
+                        <span className="fw-bolder text-primary">Rec-Review</span>
+                    </a>
+
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">contacts</a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
         </header>
+
     )
 
 }
--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,9 +1,28 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+//import { useParams, Link } from "react-router-dom"
+
 export default function Info() {
+
+    //const { id } = useParams()
+    const [details, seDetails] = useState({})
+
+    function axiosCall() {
+        axios.get('http://localhost:3000/api/movies/1')
+            .then((rispo) => seDetails(rispo.data))
+            .catch((err) => alert('michè non ti vuole davvero bene'))
+
+    }
+
+    useEffect(axiosCall, [])
+
 
     return (
 
         <>
             <h2>DETTAGLI DEL PRODOTTO</h2>
+            <p>{details.title}</p>
+
         </>
     )
 }
